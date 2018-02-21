443110f74cc577c0c5266572d41cb88873c4b85c
Kris Nova <kris@nivenly.com>
2018-02-21 12:56:51 -0800

    Tweaking for call
    Signed-off-by: Kris Nova <kris@nivenly.com>

--- _deploy.sh
+++ _deploy.sh
@@ -4,6 +4,8 @@ set -e
 T=$(mktemp)
 kubectl delete deploy ${DEPLOYMENT} -n ${NAMESPACE} || true
 kubectl delete namespace heptio-contour || true
+kubectl delete svc java-prototype || true
+kubectl delete ing java-prototype || true
 cat manifests/deployment.yaml | sed "s/REGISTRY/${REGISTRY}/g" | sed "s/IMAGE/${IMAGE}/g" | sed "s/AUTHOR/${AUTHOR}/g" | sed "s/TAG/${TAG}/g" | sed "s/DEPLOYMENT/${DEPLOYMENT}/g" > ${T}
 echo "---\n" >> ${T}
 cat manifests/ingress.yaml >> ${T}
