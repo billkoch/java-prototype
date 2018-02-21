285081f8911272dbd3f4c8fa648876549d29bb95
Kris Nova <kris@nivenly.com>
2018-02-21 12:42:49 -0800

    Getting contour up and running
    Signed-off-by: Kris Nova <kris@nivenly.com>

--- _deploy.sh
+++ _deploy.sh
@@ -2,19 +2,22 @@
 set -e
 . _vars.sh
 T=$(mktemp)
-kubectl delete deploy ${DEPLOYMENT} || true
+kubectl delete deploy ${DEPLOYMENT} -n ${NAMESPACE} || true
+kubectl delete namespace heptio-contour || true
 cat manifests/deployment.yaml | sed "s/REGISTRY/${REGISTRY}/g" | sed "s/IMAGE/${IMAGE}/g" | sed "s/AUTHOR/${AUTHOR}/g" | sed "s/TAG/${TAG}/g" | sed "s/DEPLOYMENT/${DEPLOYMENT}/g" > ${T}
 echo "---\n" >> ${T}
 cat manifests/ingress.yaml >> ${T}
 echo "---\n" >> ${T}
 cat manifests/service.yaml >> ${T}
-cat ${T}
-kubectl apply -f ${T}
+echo "---\n" >> ${T}
+kubectl apply -f ${T} -n ${NAMESPACE}
+kubectl apply -f manifests/contour.yaml
 echo "|------------------------------------------------------------"
 echo "|"
 echo "| [JAVA PROTOTYPE]"
 echo "|"
 echo "| Created Kubernetes Deployment: ${DEPLOYMENT}"
+echo "| Installed Heptio Contour Ingress Controller"
 echo "|"
 echo "|------------------------------------------------------------"
 echo ""
